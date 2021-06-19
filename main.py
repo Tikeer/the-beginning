56921b63e578e6f6090eac0a86f29aeb8d1a652c
Tikeer <86082061+Tikeer@users.noreply.github.com>
2021-06-19 13:33:30 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,119 @@
+# START PYTHON BASICS   chapter one "Creating variables"
+print("hello world")
+status="watching netflix"
+print(status)
+variable=1
+variable="Hello im "
+print(variable+status)
+print("followers: " + "55")
+followers="55"
+print("followers: "+followers)
+label = "posts: " + "13"
+print(label)
+login="Name: "+"Joe"
+print(login)
+user = "snoopdogg"
+print("username: " + user )
+temperature="14"
+print(temperature+" degrees")
+print("Posts:"+" 55")
+title= "Ms. "
+name ="Irene"
+print(title+name)
+number_of_likes = "40"
+print(number_of_likes+" likes")
+# mozna wpisac variable + string tylko jestli w variable jest string czyli np ilosc_kur="15"
+#print("Tyle mamy kur "+ ilosc_kur)
+print("----------------------------------------------------------------")#first chapter ended "Using variables"
+
+# mnozenie = * dzielnie = /
+# oraz jesli chcemy napisac liczbe ze variable to piszemy , np km=14 print("km",km)
+active_users=5
+number_of_applications = 5+1
+print(number_of_applications)
+percent=0.5*100
+print(percent)
+number_of_steps = 70
+print("You're on step: ")
+print(number_of_steps+1)
+private=3
+public=10
+total= private+public
+print("Total posts: ")
+print(total)
+km=14
+print("km: ", km)
+speed=300+5
+print(speed)
+area="3*5"
+print(area)
+savings =100
+interest=0.04
+print(savings*interest)
+sum_of_grades=460
+students=5
+print(sum_of_grades/students)
+total=100
+discount=20
+print("Discounted price: ")
+print(total-discount)
+print("----------------------------------------------------------------")#chapter two ended "Using variables"
+
+# not jest to negacja np not True = False
+powered_on=True
+correct= True
+print(correct)
+print("Load data")
+status_two= False
+print(status_two)
+print("Auto update")
+print(False)
+print(not True)
+print(not False)
+available= True
+print(not available)
+morning=True
+is_evening=not morning
+print(is_evening)
+print("----------------------------------------------------------------")#chapter three ended "True and false"
+
+# == jest do porównywania liczb/zmiennych czy sa takie same
+# != jest zeby sprawdzic jesli liczba nie jest rowna do innej liczby czyli odwrotność ==
+entered_pin=5448
+saved_pin=5440
+print(entered_pin==saved_pin)
+
+print(5+13)
+print(5==13)
+
+votes=10
+print(votes==11)
+print(1!=10)
+
+result = 1!=2
+print(result)
+
+score=1==2
+print(score)
+
+one=1
+two=2
+print(one==two)
+print(one!=two)
+
+vote_count=99
+target = vote_count == 100
+print(target)
+
+orders=1000
+capacity = orders ==1000
+print(capacity)
+
+score_one =100
+score_two =80
+equal = score_one == score_two
+print(equal)
+print("----------------------------------------------------------------")#chapter four ended "Checking Number Equality"
+# KONIEC PYTHON BASICS
+
+# START Types and Comparisions
