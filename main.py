546fd0587872e28173e2b3d3a23ad5d39ad9641c
Tikeer <86082061+Tikeer@users.noreply.github.com>
2021-06-19 17:50:23 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,119 +1,196 @@
 # START PYTHON BASICS   chapter one "Creating variables"
 print("hello world")
-status="watching netflix"
+status = "watching netflix"
 print(status)
-variable=1
-variable="Hello im "
+variable_one = 1
+variable = "Hello im "
 print(variable+status)
 print("followers: " + "55")
-followers="55"
+followers = "55"
 print("followers: "+followers)
 label = "posts: " + "13"
 print(label)
-login="Name: "+"Joe"
+login = "Name: "+"Joe"
 print(login)
 user = "snoopdogg"
-print("username: " + user )
-temperature="14"
+print("username: " + user)
+temperature = "14"
 print(temperature+" degrees")
 print("Posts:"+" 55")
-title= "Ms. "
-name ="Irene"
+title = "Ms. "
+name = "Irene"
 print(title+name)
 number_of_likes = "40"
 print(number_of_likes+" likes")
 # mozna wpisac variable + string tylko jestli w variable jest string czyli np ilosc_kur="15"
-#print("Tyle mamy kur "+ ilosc_kur)
-print("----------------------------------------------------------------")#first chapter ended "Using variables"
+# print("Tyle mamy kur "+ ilosc_kur)
+print("----------------------------------------------------------------")  # first chapter ended "Using variables"
 
 # mnozenie = * dzielnie = /
 # oraz jesli chcemy napisac liczbe ze variable to piszemy , np km=14 print("km",km)
-active_users=5
+active_users = 5
 number_of_applications = 5+1
 print(number_of_applications)
-percent=0.5*100
+percent = 0.5*100
 print(percent)
 number_of_steps = 70
 print("You're on step: ")
 print(number_of_steps+1)
-private=3
-public=10
-total= private+public
+private = 3
+public = 10
+total = private+public
 print("Total posts: ")
 print(total)
-km=14
+km = 14
 print("km: ", km)
-speed=300+5
+speed = 300+5
 print(speed)
-area="3*5"
+area = "3*5"
 print(area)
-savings =100
-interest=0.04
+savings = 100
+interest = 0.04
 print(savings*interest)
-sum_of_grades=460
-students=5
+sum_of_grades = 460
+students = 5
 print(sum_of_grades/students)
-total=100
-discount=20
+total = 100
+discount = 20
 print("Discounted price: ")
 print(total-discount)
-print("----------------------------------------------------------------")#chapter two ended "Using variables"
+print("----------------------------------------------------------------")  # chapter two ended "Using variables"
 
 # not jest to negacja np not True = False
-powered_on=True
-correct= True
+powered_on = True
+correct = True
 print(correct)
 print("Load data")
-status_two= False
+status_two = False
 print(status_two)
 print("Auto update")
 print(False)
 print(not True)
 print(not False)
-available= True
+available = True
 print(not available)
-morning=True
-is_evening=not morning
+morning = True
+is_evening = not morning
 print(is_evening)
-print("----------------------------------------------------------------")#chapter three ended "True and false"
+print("----------------------------------------------------------------")  # chapter three ended "True and false"
 
 # == jest do porównywania liczb/zmiennych czy sa takie same
 # != jest zeby sprawdzic jesli liczba nie jest rowna do innej liczby czyli odwrotność ==
-entered_pin=5448
-saved_pin=5440
-print(entered_pin==saved_pin)
+entered_pin = 5448
+saved_pin = 5440
+print(entered_pin == saved_pin)
 
 print(5+13)
-print(5==13)
+print(5 == 13)
 
-votes=10
-print(votes==11)
-print(1!=10)
+votes = 10
+print(votes == 11)
+print(1 != 10)
 
-result = 1!=2
+result = 1 != 2
 print(result)
 
-score=1==2
+score = 1 == 2
 print(score)
 
-one=1
-two=2
-print(one==two)
-print(one!=two)
+one = 1
+two = 2
+print(one == two)
+print(one != two)
 
-vote_count=99
+vote_count = 99
 target = vote_count == 100
 print(target)
 
-orders=1000
-capacity = orders ==1000
+orders = 1000
+capacity = orders == 1000
 print(capacity)
 
-score_one =100
-score_two =80
+score_one = 100
+score_two = 80
 equal = score_one == score_two
 print(equal)
-print("----------------------------------------------------------------")#chapter four ended "Checking Number Equality"
+print("--------------------------------------------------------------")  # chapter four ended "Checking Number Equality"
 # KONIEC PYTHON BASICS
 
-# START Types and Comparisions
+# START Types and Comparsions
+
+# < mniejsza niż inna liczba np 1<235
+# > wieksza niz inna liczba np 235>1
+# <= mniejsza badz rowna liczba
+# >= wieksza badz rowna liczba
+print(1 < 235)
+print(235 < 1)
+print(101 > 90)
+print(1 > 1)
+print(1 <= 3)
+print(11 <= 11)
+print(3099 >= 3099)
+print(124578 >= 2)
+print(23 >= 45)
+
+result_one = 1 <= 15
+print(result_one)
+
+min = 5
+max = 10
+result_two = min <= max
+print(result_two)
+
+battery_level = 10
+low = battery_level <= 20
+print("low battery: ")
+print(low)
+
+points = 12
+level_two = points >= 10
+print("level 2: ")
+print(level_two)
+
+solved = 20
+minimum = 10
+lost_streak = solved <= minimum
+print(lost_streak)
+print("----------------------------------------------------------------")  # chapter 1 ended "comparing numbers"
+
+print("online" == "online")
+print("online" != "offline")
+print("online" == "offline")
+
+fruit_1 = "apple"
+fruit_2 = "orange"
+print(fruit_1 == fruit_2)
+
+my_answer = "act"
+solution = "ace"
+print(my_answer == solution)
+
+subscribed = "subscribed" != "rejected "
+print(subscribed)
+
+previous_leader = "anna"
+new_leader = "Jim"
+leader_changed = previous_leader != new_leader
+print(leader_changed)
+print("----------------------------------------------------------------")  # chapter 2 ended "comparing strings"
+
+# string - "przyklad"
+# integer - przyklad = 42
+# float - przyklad - 3.14159 wiecej niz jedna cyfra po kropce
+# boolean - przyklad = True / False
+# assign - is_on = False przypisujemy value to variable
+
+name = "Joey"  # string
+age = 29  # integer
+is_active = True  # boolean
+print("----------------------------------------------------------------")  # chapter 3 ended "discovering types"
+# KONIEC TYPES AND COMPARSIONS
+
+# START Conditional Statements
+
+
+
+print("----------------------------------------------------------------")  # chapter 1 ended "Formatting Strings"
